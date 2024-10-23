06a0be372f58797978a247ef95975004c5724fdf
carlos terrazas <carlos.terrazasv@gmail.com>
2024-10-23 00:04:22 -0300

    bug

---
+++ src/app/fet/page.tsx
@@ -0,0 +1,5 @@
+/** @format */
+import { Button } from '@material-tailwind/react';
+export default async function page() {
+    return <Button color="warning">Button</Button>;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
-import type { Config } from "tailwindcss";
+/** @format */
+
+import type { Config } from 'tailwindcss';
+
+import { mtConfig } from '@material-tailwind/react';
+
+// const { mtConfig } = require('@material-tailwind/react/');
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+        './node_modules/@material-tailwind/react/**/*.{js,ts,jsx,tsx}',
+    ],
+
+    plugins: [mtConfig],
 };
+
 export default config;
